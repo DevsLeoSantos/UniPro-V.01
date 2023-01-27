9433060f7444008d93120347f6069586e2026327
Leonardo <leonardobarrochello58@gmail.com>
2023-01-27 10:53:13 -0300

    salvar contato

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 //Bibliotecas
-import Axios from "axios"
-import BntWhats from "./Components/BntWhats"
+import Axios from "axios";
+import BntWhats from "./Components/BntWhats";
 
 //Componentes
-import Footer from './Components/Fotter'
-import NavBar from './Components/NavBar'
+import Footer from "./Components/Fotter";
+import NavBar from "./Components/NavBar";
 
 //Estilos
-import './App.css'
+import "./App.css";
 
 //teste
-import Login from "./Pages/Login/Index"
-import DashBoard from "./Pages/DashBoard/Index"
-
+import Login from "./Pages/Login/Index";
+import DashBoard from "./Pages/DashBoard/Index";
 
 function App() {
-
   return (
     <div className="App">
       <NavBar />
       <Footer />
-      <BntWhats/>
+      <BntWhats />
       {/* <Login/> */}
       {/* <DashBoard/> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
